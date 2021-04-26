16e9e1aaf3a9a64b1a5800cee6dc874101272b1b
Rodrigo Dias Soares <rodrigodiassoares19@gmail.com>
2021-04-26 20:25:31 -0300

    Criando estruturas basicas do projeto

---
+++ app/src/main/java/com/example/convidados/ui/slideshow/AbsentViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.convidados.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AbsentViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Absent Fragment"
+    }
+    val text: LiveData<String> = _text
+}
