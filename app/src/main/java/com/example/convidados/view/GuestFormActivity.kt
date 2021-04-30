05e87d811dcb5366fe518c215244931a20c3c4da
Rodrigo Dias Soares <rodrigodiassoares19@gmail.com>
2021-04-29 22:43:22 -0300

    Criado metodos para a manipulação dos dados do SQLite

--- app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
@@ -39,6 +39,7 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
             }else{
                 Toast.makeText(applicationContext, "Falha", Toast.LENGTH_SHORT).show()
             }
+            finish()
         })
     }
 
--- app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
@@ -1,18 +1,22 @@
 package com.example.convidados.viewmodel
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.convidados.service.model.GuestModel
 import com.example.convidados.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel(){
-    private val mGuestRepository: GuestRepository = GuestRepository()
+class GuestFormViewModel(application: Application) : AndroidViewModel(application){
+    private val mGuestRepository: GuestRepository =
+        GuestRepository.getInstance(application.applicationContext)
     private var mSaveGest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGest
 
     fun save(name: String, presence: Boolean){
-        val guest = GuestModel(name, presence)
-        mGuestRepository.save(guest)
+        val guest = GuestModel(name = name, presence =  presence)
+        mSaveGest.value = mGuestRepository.save(guest)
     }
 }
