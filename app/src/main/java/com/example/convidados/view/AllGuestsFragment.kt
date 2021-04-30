592bf1cb10d152ae4bc9db42a01ebfe599c9029c
Rodrigo Dias Soares <rodrigodiassoares19@gmail.com>
2021-04-30 00:15:11 -0300

    Criado RecyclerView  e listando itens salvos

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -4,29 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.R
+import com.example.convidados.view.adapter.GuestAdapter
 import com.example.convidados.viewmodel.AllGuestsViewModel
+
 
 class AllGuestsFragment : Fragment() {
 
-    private lateinit var homeViewModel: AllGuestsViewModel
+    private lateinit var allGuestsViewModel: AllGuestsViewModel
+    private val mAdapter: GuestAdapter = GuestAdapter()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(AllGuestsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        allGuestsViewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_all, container, false)
+
+        val recycler = root.findViewById<RecyclerView>(R.id.recycler_all_guests)
+
+        recycler.layoutManager = LinearLayoutManager(context)
+
+        recycler.adapter = mAdapter
+
+        observer()
+
+
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        allGuestsViewModel.load()
+    }
+
+    private fun observer(){
+        allGuestsViewModel.guestList.observe(viewLifecycleOwner, Observer {
+            mAdapter.updateGuests(it)
+        })
     }
 }
